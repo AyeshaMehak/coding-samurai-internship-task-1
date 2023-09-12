f9ed8073afa92f892389dc6acb11315860a8edc6
AyeshaMehak <aishamehak15@gmail.com>
2023-09-12 19:13:16 +0500

    public/images/corner1.png

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,220 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { useState } from "react";
+import { FiDelete } from "react-icons/fi";
+import { FaDivide } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+import React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+export default function Calculator() {
+ const [displayValue, setDisplayValue] = useState("0");
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+ function handleButtonClick(value) {
+  if (value === "C") {
+   // Clear the whole input
+   setDisplayValue("0");
+  } else if (value === "D") {
+   // Delete the previous character (backspace)
+   setDisplayValue(displayValue.slice(0, -1));
+  } else {
+   // Append the clicked value
+   if (displayValue === "0" && value !== "C") {
+    setDisplayValue(value);
+   } else {
+    setDisplayValue(displayValue + value);
+   }
+  }
+ }
+
+ const calculateResult = () => {
+  try {
+   // Use JavaScript's eval to calculate the result
+   const result = eval(displayValue);
+   setDisplayValue(result.toString());
+  } catch (error) {
+   setDisplayValue("Invalid value");
+  }
+ };
+ return (
+  <>
+   <section className="min-h-screen flex justify-center items-center">
+    {/* calculator bg div */}
+    <div className="shadow2 sm:w-[25rem] sm:h-[35rem] w-[20rem] bg-transparent border border-gray-600 rounded-3xl drop-shadow-2xl">
+     {/* output div  */}
+     <div className="flex justify-center">
+      <input
+       className="w-[19rem] h-[8rem] bg-transparent rounded-xl my-5 text-right text-white text-5xl font-semibold placeholder-[baseline] relative"
+       value={displayValue}
+       readOnly
+      />
+     </div>
+     {/* buttons row1  */}
+     <div className="flex items-center justify-center my-2">
+      {/* Clear */}
+      <button
+       className="shadow1 bg-white mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-orange-600  relative font-semibold text-lg"
+       onClick={() => handleButtonClick("C")}
+      >
+       C
+      </button>
+
+      {/* delete x */}
+      <button
+       className="shadow1 bg-white mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-orange-600  relative font-bold text-xl"
+       onClick={() => handleButtonClick("D")}
+      >
+       <FiDelete className="" />
+      </button>
+
+      {/* divide /  */}
+      <button
+       className="shadow1 bg-white mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-orange-600  relative font-thin"
+       onClick={() => handleButtonClick("/")}
+      >
+       <FaDivide />
+      </button>
+      {/* multiply * */}
+      <button className="shadow1 bg-white mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-orange-600  relative">
+       <RxCross2 className="text-lg" onClick={() => handleButtonClick("*")} />
+      </button>
+     </div>
+
+     {/* buttons row-2  */}
+     {/* 07 */}
+     <div className="flex items-center justify-center my-2">
+      <button
+       className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-gray-700 hover:text-black"
+       onClick={() => handleButtonClick("7")}
+      >
+       7
+      </button>
+      {/* 08 */}
+      <button
+       className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-gray-700 hover:text-black"
+       onClick={() => handleButtonClick("8")}
+      >
+       8
+      </button>
+      {/* 09 */}
+      <button
+       className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-gray-700 hover:text-black"
+       onClick={() => handleButtonClick("9")}
+      >
+       9
+      </button>
+      {/* subtraction - */}
+      <button
+       className="shadow1 text-3xl bg-white mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-orange-600  relative font-semibold "
+       onClick={() => handleButtonClick("-")}
+      >
+       -
+      </button>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+     {/* buttons row-3  */}
+     {/* 04*/}
+     <div className="flex items-center justify-center my-2">
+      <button
+       className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-gray-700 hover:text-black"
+       onClick={() => handleButtonClick("4")}
+      >
+       4
+      </button>
+      {/* 05 */}
+      <button
+       className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-gray-700 hover:text-black"
+       onClick={() => handleButtonClick("5")}
+      >
+       5
+      </button>
+      {/* 06 */}
+      <button
+       className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-gray-700 hover:text-black"
+       onClick={() => handleButtonClick("6")}
+      >
+       6
+      </button>
+      {/* addition + */}
+      <button
+       className="shadow1 bg-white mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-orange-600  relative text-2xl"
+       onClick={() => handleButtonClick("+")}
+      >
+       +
+      </button>
+     </div>
+
+     {/* buttons row-4  */}
+     {/* 01 */}
+     <div className="flex items-center justify-center my-2">
+      <button
+       className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-gray-700 hover:text-black"
+       onClick={() => handleButtonClick("1")}
+      >
+       1
+      </button>
+      {/* 02 */}
+      <button
+       className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-gray-700 hover:text-black"
+       onClick={() => handleButtonClick("2")}
+      >
+       2
+      </button>
+      {/* 03 */}
+      <button
+       className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-gray-700 hover:text-black"
+       onClick={() => handleButtonClick("3")}
+      >
+       3
+      </button>
+       {/* decimal . */}
+       <button
+        className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center font-bold text-xl duration-200 text-gray-700 hover:text-black"
+        onClick={() => handleButtonClick(".")}
+       >
+        .
+       </button>
+     </div>
+
+     {/* buttons row-5  */}
+     <div className="flex items-center justify-center my-2">
+     {/* double zero 00 */}
+       <button
+        className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-gray-700 hover:text-black"
+        onClick={() => handleButtonClick("00")}
+       >
+        00
+       </button>
+       {/* zero 0 */}
+       <button
+        className="shadow1 bg-gradient-to-br from-gray-300 via-gray-400 to-white relative mx-2 inline-flex p-5 rounded-full w-[4rem] h-[4rem] items-center text-center justify-center duration-200 text-gray-700 hover:text-black"
+        onClick={() => handleButtonClick("0")}
+       >
+        0
+       </button>
+       {/* equals = */}
+       <button
+        className="shadow1 bg-white mx-2 inline-flex p-5 rounded-full w-[9rem] h-[4rem] items-center text-center justify-center duration-200 text-orange-600  relative text-2xl font-bold"
+        onClick={calculateResult}
+       >
+        =
+       </button>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+     </div>
+   </section>
+   <Image
+    src={require("../public/images/corner1.png")}
+    className="absolute top-0 left-0 xl:w-[24rem] sm:w-[19rem] max-sm:hidden "
+    width={400}
+    height={400}
+    alt="border"
+   />
+   <Image
+    src={require("../public/images/corner1.png")}
+    className="absolute bottom-0 right-0 rotate-180 xl:w-[24rem] sm:w-[19rem] max-sm:hidden "
+    width={400}
+    height={400}
+    alt="border"
+   />
+  </>
+ );
 }
